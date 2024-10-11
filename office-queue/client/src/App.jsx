dbb9de28e5a41c298893919a0c081e8d4a95996f
Nicolai Faye <nicohfaye@gmail.com>
2024-10-11 10:10:50 +0200

    Implementing DisplayBoard and CurrentTickets

--- office-queue/client/src/App.jsx
+++ office-queue/client/src/App.jsx
@@ -1,26 +1,10 @@
-import { useState } from "react";
 import "./App.css";
+import DisplayBoard from "./components/DisplayBoard";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <div className="flex w-full h-full bg-slate-600">Hello</div>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <DisplayBoard />
     </>
   );
 }
--- office-queue/client/src/components/CurrentTickets.jsx
+++ office-queue/client/src/components/CurrentTickets.jsx
@@ -1,5 +1,69 @@
+import { useState } from "react";
+
+const mockTickets = [
+  {
+    ticket_id: "1",
+    service_name: "Shipping",
+    counter_id: "1",
+    issued_time: "2024-10-10T09:55:00",
+  },
+  {
+    ticket_id: "2",
+    service_name: "Shipping",
+    counter_id: "1",
+    issued_time: "2024-10-10T09:55:00",
+    status: "WAITING",
+  },
+  {
+    ticket_id: "3",
+    service_name: "",
+    counter_id: "1",
+    issued_time: "2024-10-10T09:55:00",
+    status: "WAITING",
+  },
+];
+
 const CurrentTickets = () => {
-  return <>Hello</>;
+  const [currentTickets, setCurrentTickets] = useState([]);
+
+  setCurrentTickets(mockTickets);
+
+  //   useEffect(() => {
+  //     // Function to fetch current tickets
+  //     const fetchCurrentTickets = () => {
+  //       fetch('http://localhost:3000/api/current-tickets')
+  //         .then((response) => response.json())
+  //         .then((data) => setCurrentTickets(data))
+  //         .catch((error) => console.error('Error fetching current tickets:', error));
+  //     };
+
+  return (
+    <div className="w-1/2 h-1/2 bg-slate-800">
+      <h2 className="text-emerald-50">Now Serving</h2>;
+      {currentTickets.length === 0 ? (
+        <p>No tickets are currently being served.</p>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>Ticket Number</th>
+              <th>Service</th>
+              <th>Counter</th>
+            </tr>
+          </thead>
+          <tbody>
+            {currentTickets.map((ticket) => (
+              <tr key={ticket.ticket_id}>
+                <td>{ticket.ticket_id}</td>
+                <td>{ticket.service_name}</td>
+                <td>{ticket.counter_id}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
 };
 
 export default CurrentTickets;
